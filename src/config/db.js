488341e0630f39906e985589de6bf916b39fd169
HADES248 <shivasingh24803@gmail.com>
2026-04-04 12:42:02 +0530

    Refactor database connection logic to use cached connection and promise

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  if (isConnected) return;
+  if (cached.conn) return cached.conn;
 
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URI);
-    isConnected = db.connections[0].readyState === 1;
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error(error);
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => {
+      return mongoose;
+    });
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectDB;
